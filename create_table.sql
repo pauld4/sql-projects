3e52f0e24b1ca36d564911db00657bd956179bd9
Paul Dziedzic <53009545+pauld4@users.noreply.github.com>
2026-02-02 15:37:02 -0500

    Add files via upload

---
+++ create_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE accounts (
+    account_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    email VARCHAR(128) NOT NULL UNIQUE,
+    display_name VARCHAR(16) NOT NULL,
+    key1 CHAR(32) NOT NULL,
+    key2 CHAR(32) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    salt CHAR(32) NOT NULL,
+    account_access TINYINT UNSIGNED NOT NULL,
+	created_on INT UNSIGNED NOT NULL,
+
+    PRIMARY KEY (account_id)
+) ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4;
